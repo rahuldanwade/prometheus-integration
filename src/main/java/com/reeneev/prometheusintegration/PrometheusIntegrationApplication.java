a909b828d078e0384524c26c780aa3c59ece7156
Danwade, Rahul <rahul.danwade@volvocars.com>
2022-12-28 13:17:20 +0100

    Initial commit

---
+++ src/main/java/com/reeneev/prometheusintegration/PrometheusIntegrationApplication.java
@@ -0,0 +1,27 @@
+package com.reeneev.prometheusintegration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class PrometheusIntegrationApplication {
+	final static Logger logger = LoggerFactory.getLogger(
+			PrometheusIntegrationApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PrometheusIntegrationApplication.class, args);
+	}
+
+	@GetMapping("/something")
+	public ResponseEntity<String> createLogs() {
+		logger.warn("Just checking");
+		return ResponseEntity.ok().body("All Ok");
+	}
+}
---
+++ src/test/java/com/reeneev/prometheusintegration/PrometheusIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reeneev.prometheusintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
